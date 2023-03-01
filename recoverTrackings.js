27b61d49388ff49035553e9e02e9bc1fa7f012e4
André Júnior <57339312+Andre1999Lopes@users.noreply.github.com>
2023-03-01 19:11:06 -0300

    Add files via upload

--- recoverTrackings.js
+++ recoverTrackings.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
+const MERGE = "MergeContact";
+const TRACK = "TrackEvent";
+const ENTERING_ACTIONS = "$enteringCustomActions";
+const LEAVING_ACTIONS = "$leavingCustomActions";
+const optionsJson = {
+	"tracking": TRACK,
+	"merge": MERGE
+}
 
-const [node, archive, botOrigin, botDestination] = process.argv;
+const option = TRACK;
+
+const [node, archive, botOrigin, botDestination, action] = process.argv;
 
 if (!botOrigin || !botDestination) {
 	console.log("botOrigin and botDestination can't be null");
 	return;
 }
+
+if (action && optionsJson[action]) option = optionsJson[action];
+
 
 let raw = fs.readFileSync(`${botOrigin}`);
 let json = JSON.parse(raw);
@@ -16,19 +29,31 @@ flowKeys.forEach(key => {
 	let blockName = json["flow"][key]["$title"];
 	newJson[blockName] = [];
 
-	json["flow"][key]["$enteringCustomActions"].forEach(action => {
-		if (action["type"] == "TrackEvent") newJson[blockName].push(action["$title"]);
-	})
+	if (option == TRACK) {
+		json["flow"][key][ENTERING_ACTIONS].forEach(action => {
+			if (action["type"] == TRACK) newJson[blockName].push(action["$title"]);
+		})
+	
+		json["flow"][key][LEAVING_ACTIONS].forEach(action => {
+			if (action["type"] == TRACK) newJson[blockName].push(action["$title"]);
+		})
+	}
 
-	json["flow"][key]["$leavingCustomActions"].forEach(action => {
-		if (action["type"] == "TrackEvent") newJson[blockName].push(action["$title"]);
-	})
+	else if (option == MERGE) {
+		json["flow"][key][ENTERING_ACTIONS].forEach(action => {
+			if (action["type"] == MERGE) newJson[blockName].push(action["settings"]);
+		})
+	
+		json["flow"][key][LEAVING_ACTIONS].forEach(action => {
+			if (action["type"] == MERGE) newJson[blockName].push(action["settings"]);
+		})
+	}
 })
 
 newJsonKeys = Object.keys(newJson)
 newJsonKeys.forEach(key => {
 	if (newJson[key].length == 0) delete newJson[key];
 })
-
+ 
 let data = JSON.stringify(newJson);
 fs.writeFileSync(`${botDestination}`, data);
